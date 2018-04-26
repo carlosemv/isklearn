77ec621b15019d7302b59dac0250370381fa9752
CARLOS EDUARDO MORAIS VIEIRA <cemvieira@service0.smc-default.imd.local>
2018-04-26 13:49:13 -0300

    first tests

---
+++ make_instances.py
@@ -0,0 +1,11 @@
+import pandas as pd
+from random import sample
+
+num_instances = 10000
+instances = []
+
+for i in range(num_instances):
+	instances.append(sample(range(1,18), 3))
+
+df = pd.DataFrame(instances)
+df.to_csv("instances.txt", header=False, index=False)
