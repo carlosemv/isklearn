9234659453814757e9eeac7ceebc0dc47ab5c153
Carlos Vieira <vieiramecarlos@gmail.com>
2020-01-21 19:52:34 -0300

    Update instances gen script

--- make_instances.py
+++ make_instances.py
@@ -1,11 +1,19 @@
+import itertools
 import pandas as pd
-from random import sample
+from random import sample, shuffle
 
-num_instances = 10000
-instances = []
+def triple_instances(folds, samples=500):
+	instances = list(itertools.combinations(range(folds), 3))
+	if samples and samples < len(instances):
+		instances = sample(instances, samples)
+	return instances
 
-for i in range(num_instances):
-	instances.append(sample(range(1,10), 3))
+def single_instances(folds):
+	instances = list(range(folds))
+	shuffle(instances)
+	return instances
 
+
+instances = triple_instances(20)
 df = pd.DataFrame(instances)
-df.to_csv("instances.txt", header=False, index=False)
+df.to_csv("new-instances.txt", header=False, index=False)
